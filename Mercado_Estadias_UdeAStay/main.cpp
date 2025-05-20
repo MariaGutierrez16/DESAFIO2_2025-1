3ce2c7a029142e5780d821afca65e2b5fc5eb900
AlejandraG <maria.gutierrezr1@udea.edu.co>
2025-05-19 20:39:04 -0500

    19_05_2025_1

--- Mercado_Estadias_UdeAStay/main.cpp
+++ Mercado_Estadias_UdeAStay/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
-}
+// ---------------- Clase Fecha ----------------
+class Fecha {
+public:
+    int dia, mes, anio;
+
+    Fecha() : dia(1), mes(1), anio(2023) {}
+    Fecha(int d, int m, int a) : dia(d), mes(m), anio(a) {}
+
+    bool validar() {
+        if (mes < 1 || mes > 12 || dia < 1 || dia > 31) return false;
+        if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) return false;
+        if (mes == 2) {
+            bool bisiesto = (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0));
+            if (dia > (bisiesto ? 29 : 28)) return false;
+        }
+        return true;
+    }
+};
+
